5fd01bb00d5afb9f63db08ed29e5acb979574a65
Gabriel Andretta <ohhgabriel@gmail.com>
2011-05-10 11:59:49 -0700

    Let the model args take priority over the env ones

--- lib/paperclip/env_aware/active_record/class_methods.rb
+++ lib/paperclip/env_aware/active_record/class_methods.rb
@@ -2,7 +2,7 @@ module Paperclip::EnvAware
   module ActiveRecord
     module ClassMethods
       def has_attached_file(*args)
-        opts = ConfigReader.read('paperclip_env.yml').reverse_merge(args.extract_options!)
+        opts = ConfigReader.read('paperclip_env.yml').merge(args.extract_options!)
         args.push(opts.symbolize_keys)
         super(*args)
       end
